c2a2953bf11facfcf3730d91889a192cf2d06852
Hector <hsandoval79@gmail.com>
2021-05-31 12:45:15 -0700

    FizzBuzz - Fully implemented function

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,14 +1,18 @@
-def fizzbuzz(num):
-    if (num > 0 and num <= 100):
-        if (num % 5 == 0):
-            if (num % 3 == 0):
-                return "FizzBuzz"
+def fizzbuzz():
+    num = 1
+    while num <= 100:
+        if (num > 0 and num <= 100):
+            if (num % 5 == 0):
+                if (num % 3 == 0):
+                    print ("FizzBuzz")
+                else:
+                    print ("Buzz")
             else:
-                return "Buzz"
-        else:
-            if (num % 3 == 0):
-                return "Fizz"
-            else:
-                return num
-    else:
-        return None
+                if (num % 3 == 0):
+                    print ("Fizz")
+                else:
+                    print (num)
+        num = num + 1
+        #else:
+            #return None
+fizzbuzz()
