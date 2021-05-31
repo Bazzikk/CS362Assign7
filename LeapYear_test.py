62d31f06c69bf9baf3ba377e53742aac927d6882
Hector <hsandoval79@gmail.com>
2021-05-31 13:05:32 -0700

    LeapYear - Second test write up, verifying number is divisible by 4 and 100

--- LeapYear_test.py
+++ LeapYear_test.py
@@ -4,3 +4,6 @@ import LeapYear
 class TestClass:
     def test_one(self):
         assert LeapYear.leapYearVerifier(4) == True
+
+    def test_two(self):
+        assert LeapYear.leapYearVerifier(100) == True
