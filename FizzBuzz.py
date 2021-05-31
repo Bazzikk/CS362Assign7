e73a35168a893909b8333d858bcbc5f1762f926d
Hector <hsandoval79@gmail.com>
2021-05-31 12:34:18 -0700

    Implemented test 5, verifying number divisible by 5 print 'Buzz'

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,13 +1,13 @@
 def fizzbuzz(num):
     if (num > 0 and num <= 100):
-        if (num % 5 == 0):
-            if (num % 3 == 0):
+        if (num % 3 == 0):
+            if (num % 5 == 0):
                 return "FizzBuzz"
             else:
-                return "Buzz"
-        else:
-            if (num % 3 == 0):
                 return "Fizz"
+        else:
+            if (num % 5 == 0):
+                return "Buzz"
             else:
                 return num
     else:
