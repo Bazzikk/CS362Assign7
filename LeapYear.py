6d7e28a167f69b6b74bd6ed390a1075183712bef
Hector <hsandoval79@gmail.com>
2021-05-31 13:14:41 -0700

    Implemented test 3, verifying number divisible by 4, 100, and 400 return true

--- LeapYear.py
+++ LeapYear.py
@@ -2,6 +2,7 @@
 def leapYearVerifier (year):
     if (year % 4 == 0):
         if (year % 100 == 0):
-            return False
+            if (year % 400 == 0):
+                return True
         else:
             return True
