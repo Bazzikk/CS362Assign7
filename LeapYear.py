c7eb988eb3bc6f790f30bc9cd8cbd1d0d901ed6e
Hector <hsandoval79@gmail.com>
2021-05-31 13:18:10 -0700

    LeapYear - Fully implemented function

--- LeapYear.py
+++ LeapYear.py
@@ -3,6 +3,19 @@ def leapYearVerifier (year):
     if (year % 4 == 0):
         if (year % 100 == 0):
             if (year % 400 == 0):
-                return True
+                print("It is a leap year")
+            else:
+                print("Not a leap year")
+
         else:
-            return True
+            print("It is a leap year")
+
+    else:
+        print("Not a leap year")
+
+# Enter year to verify
+val = input("Enter year: ")
+
+print("Verifying if year {} is a leap year".format(val))
+
+leapYearVerifier(int(val))
